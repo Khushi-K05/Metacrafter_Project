63d20a1236b47fa16885b9faa6b13ae20473afb2
Khushi-K05 <22BCS10468@cuchd.in>
2024-06-28 13:05:42 +0530

    Update and rename Module2_Project.sol to Bank.sol

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SimpleBank {
+contract Bank {
     mapping(address => uint256) private balances;
 
     event Deposited(address indexed account, uint256 amount);
@@ -18,10 +18,10 @@ contract SimpleBank {
     function withdraw(uint256 _amount) public {
         require(_amount > 0, "Withdrawal amount must be greater than zero");
         require(balances[msg.sender] >= _amount, "Insufficient balance");
-        
+
         balances[msg.sender] -= _amount;
         payable(msg.sender).transfer(_amount);
-        
+
         emit Withdrawn(msg.sender, _amount);
     }
 
